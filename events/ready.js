2f34a9ba844eda06c59a8251a3b3a08bb6db8a6c
mistvle <mistle.roblox@gmail.com>
2026-06-11 22:47:32 -0700

    A

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
   async execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
 
-    client.user.setActivity("💻 Seattle Roleplay", {
+    client.user.setActivity("💻 discord.gg/sstle", {
       type: 3 // WATCHING
     });
   }
